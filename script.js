b526a3f8e3530d3d3c3c9d215c75b8b95d53dfe1
unknown <broncoscorpio@gmail.com>
2018-05-04 11:14:37 -0600

    Add basic template and try getting json object.

--- script.js
+++ script.js
@@ -6,3 +6,21 @@
 //&prop=info&format=json
 
 //I think that should work
+
+var content;
+var wurl = "https://en.wikipedia.org/w/api.php?action=query&generator=search&grsearch=butterfly&prop=info&format=json";
+
+$(document).ready(function() {
+
+    content = $("#content");
+
+    $.ajax({
+        dataType: "json",
+        url: wurl,
+        success: function(json) {
+            content.html(JSON.stringify(json));
+        }
+
+    });
+
+});
