b2f2dbafe36773084e82eaf1846c58b3972206ad
unknown <broncoscorpio@gmail.com>
2018-05-04 11:02:09 -0600

    Start wiki-app project with new files.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Endpoint for the wiki web API
+
+//https://en.wikipedia.org/w/api.php
+//?action=query&generator=search
+//&grsearch="user input text"
+//&prop=info&format=json
+
+//I think that should work
