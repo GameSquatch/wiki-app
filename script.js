f8f25c3f4051df86dd475d71e2231ee51a2b787e
unknown <broncoscorpio@gmail.com>
2018-05-04 19:37:58 -0600

    Fix anchor href bug. Now goes to correct link.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 //I think that should work
 
 var content;
+var textSearch;
 var queryArray;
 var searchWord = "butterfly";
 var wurl = "https://en.wikipedia.org/w/api.php"
@@ -16,6 +17,7 @@ var wurl = "https://en.wikipedia.org/w/api.php"
 $(document).ready(function() {
 
     content = $("#content");
+    textSearch = $("#textSearch");
 
 });
 
@@ -23,17 +25,17 @@ function createArticleDiv(title, snippet, pageid) {
     var div = "<div class='row'>"
                 + "<div class='col-3'></div>"
                 + "<div class='col-6'><strong>" + title
-                + "</strong><p><a href='https://en.wikipedia.org/wiki/Translation?curid='"
-                + pageid + " target='_blank'>" + snippet + "</a></p></div>"
+                + "</strong><p><a href='https://en.wikipedia.org/?curid="
+                + pageid + "' target='_blank'>" + snippet + "</a></p></div>"
                 + "<div class='col-3'></div>";
 
     $("#content").append(div);
 }
 
 function search() {
-    var textField = $("#textSearch").val();
+    var textField = textSearch.val();
     wurl += textField;
-    console.log(textField);
+    console.log(wurl);
 
     $.ajax({
         dataType: "jsonp",
