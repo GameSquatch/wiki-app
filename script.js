d0328be5a3a0dcb503460a39687bf34bd55ff499
unknown <broncoscorpio@gmail.com>
2018-05-04 19:29:26 -0600

    Add search and button functionality and some formatting.

--- script.js
+++ script.js
@@ -10,30 +10,44 @@
 var content;
 var queryArray;
 var searchWord = "butterfly";
-var wurl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchWord;
+var wurl = "https://en.wikipedia.org/w/api.php"
+            + "?action=query&format=json&list=search&srsearch=";
 
 $(document).ready(function() {
 
     content = $("#content");
 
+});
+
+function createArticleDiv(title, snippet, pageid) {
+    var div = "<div class='row'>"
+                + "<div class='col-3'></div>"
+                + "<div class='col-6'><strong>" + title
+                + "</strong><p><a href='https://en.wikipedia.org/wiki/Translation?curid='"
+                + pageid + " target='_blank'>" + snippet + "</a></p></div>"
+                + "<div class='col-3'></div>";
+
+    $("#content").append(div);
+}
+
+function search() {
+    var textField = $("#textSearch").val();
+    wurl += textField;
+    console.log(textField);
+
     $.ajax({
         dataType: "jsonp",
         url: wurl,
         success: function(json) {
-            content.html(JSON.stringify(json));
+            //content.html(JSON.stringify(json));
             queryArray = json["query"]["search"];
 
             for (var i = 0; i < queryArray.length; ++i) {
-                createArticleDiv(queryArray[i]["title"], queryArray[i]["snippet"], queryArray[i]["pageid"]);
+                createArticleDiv(queryArray[i]["title"]
+                                , queryArray[i]["snippet"]
+                                , queryArray[i]["pageid"]);
             }
         }
 
     });
-
-});
-
-function createArticleDiv(title, snippet, pageid) {
-    var div = "<div class='article'><strong>" + title + "</strong><p><a href='https://en.wikipedia.org/wiki/Translation?curid='" + pageid + " target='_blank'>" + snippet + "</a></p></div>";
-
-    $("#content").append(div);
 }
